758bd6d9ce6ae9f56cb9a6a7637a731ddd03ef31
Nic Netzel and torrence stratton <tlstratton@gmail.com>
2016-01-13 11:21:50 -0800

    add jscript functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,39 @@
 $(document).ready(function() {
+  $("form#game-selector").submit(function(event) {
+    var name = $("input#name").val();
+    var age = parseInt($("input#age").val());
+    var players = parseInt($("input#players").val());
+    var experience = $("input#check").prop('checked');
 
+    $(".nameInput").text(name);
 
+    if (name) {
 
+      if (age >= 15 && experience === true) {
+        var game = "Agricola";
+        $(".gameOutput").text(game);
+
+     } else if (age < 15 && players >= 5){
+       var game = "The MetaGame";
+       $(".gameOutput").text(game);
+
+     } else if (players === 3) {
+        var game = "Hive";
+        $(".gameOutput").text(game);
+
+      } else {
+        var game = "Carcasonne";
+        $(".gameOutput").text(game);
+      }
+   }
+    else {
+     alert('Please enter your name.');
+    }
+
+  $("#output-message").show();
+ event.preventDefault();
 });
+
+
+
+  });
